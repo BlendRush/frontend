960fb7e652e3502adaf5e852368cb410bcbcec99
supunijayasinghe <supuninavodya695@gmail.com>
2025-08-24 10:06:44 +0530

    change the instance

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,24 +1,39 @@
 provider "aws" {
-  region = var.region
+  region = "us-east-1"
 }
 
-resource "aws_key_pair" "frontend_key" {
-  key_name   = "frontend_key"
-  public_key = file("${path.module}/frontend_key.pub")
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "ami" {
+  # Ubuntu Server 22.04 LTS - us-east-1
+  default = "ami-053b0d53c279acc90"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+resource "aws_key_pair" "blendrush_frontend_key" {
+  key_name   = "blendrush_frontend_key"
+  public_key = file("${path.module}/blendrush_frontend_key.pub")
 }
 
 resource "aws_security_group" "frontend_sg" {
-  name        = "frontend-sg"
-  description = "Allow HTTP and SSH"
-  
+  name        = "frontend_sg"
+  description = "Allow SSH and HTTP"
+
   ingress {
+    description = "SSH"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  
+
   ingress {
+    description = "HTTP"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
@@ -33,29 +48,34 @@ resource "aws_security_group" "frontend_sg" {
   }
 }
 
-resource "aws_instance" "frontend_ec2" {
-  ami                    = var.ami
-  instance_type          = var.instance_type
-  key_name               = aws_key_pair.frontend_key.key_name
-  vpc_security_group_ids = [aws_security_group.frontend_sg.id]
+resource "aws_instance" "frontend_instance" {
+  ami                         = var.ami
+  instance_type               = var.instance_type
+  key_name                    = aws_key_pair.blendrush_frontend_key.key_name
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.frontend_sg.id]
+
+  tags = {
+    Name = "FrontendAppInstance"
+  }
 
   provisioner "remote-exec" {
     inline = [
       "sudo apt update -y",
       "sudo apt install docker.io -y",
       "sudo systemctl start docker",
-      "sudo usermod -aG docker ubuntu"
+      "sudo systemctl enable docker"
     ]
 
     connection {
       type        = "ssh"
       user        = "ubuntu"
-      private_key = file("${path.module}/frontend_key")
+      private_key = file("${path.module}/blendrush_frontend_key")
       host        = self.public_ip
     }
   }
+}
 
-  tags = {
-    Name = "Frontend-EC2"
-  }
+output "frontend_public_ip" {
+  value = aws_instance.frontend_instance.public_ip
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,3 @@
-output "frontend_public_ip" {
-  value = aws_instance.frontend_ec2.public_ip
+output "blendrush_frontend_public_ip" {
+  value = aws_instance.frontend_instance.public_ip
 }
