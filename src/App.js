25f04ff96dbac53f0b0cd64678c33cba26b2c2e6
ImashaNawodi <imashanaw1999@gmail.com>
2025-09-05 10:05:07 +0530

    fix user privilages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import { useContext } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./Pages/HomePage.jsx";
 import SignIn from "./Pages/SignIn";
@@ -8,7 +8,7 @@ import ForgotPW from "./Pages/forgot-pw.jsx";
 import Menu from "./Pages/Menu.jsx";
 import Cart from "./Pages/Cart.jsx";
 import Orders from "./Pages/Order.jsx";
-import About from "./Pages/About.jsx"; // Assuming you have an About page
+import About from "./Pages/About.jsx"; 
 import { AuthContext } from "./context/AuthContext.jsx";
 import ResetPW from "./Pages/reset-pw.jsx";
 
@@ -23,17 +23,17 @@ export default function App() {
       <Route path="/register" element={<Register />} />
       <Route path="/forgot-pw" element={<ForgotPW />} />
       <Route path="/reset-pw" element={<ResetPW />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/menu" element={<Menu />} />
 
       {token !== null && token !== "" ? (
         <>
-          <Route path="/menu" element={<Menu />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/orders" element={<Orders />} />
-          <Route path="/about" element={<About />} />
           <Route path="*" element={<div>Not Found</div>} />
         </>
       ) : (
-        <Route path="*" element={<Navigate to="/sign-in" />} />
+        <Route path="*" element={<Navigate to="/home" />} />
       )}
     </Routes>
   );
